5be0c5d2fe772bf0567c34b41e46e0be08319d64
Gianpiero Di Blasi <32394331+gianpierodiblasi@users.noreply.github.com>
2022-04-07 22:41:33 +0200

    test

--- src/test/java/giada/josetta/JosettaTest.java
+++ src/test/java/giada/josetta/JosettaTest.java
@@ -2,15 +2,22 @@ package giada.josetta;
 
 public class JosettaTest {
 
+  private static String[] args1 = new String[]{
+    "-in",
+    "C:\\Users\\gianpiero.di.blasi\\codice\\Personale\\pizzApazzAinfinity\\src\\giada\\pizzapazza\\math\\Z4Sign.java",
+    "-out",
+    "C:\\Users\\gianpiero.di.blasi\\codice\\Personale\\pizzApazzAinfinity\\transpile\\giada\\pizzapazza\\math\\Z4Sign.js"
+  };
+
+  private static String[] args2 = new String[]{
+    "-in",
+    "C:\\Users\\gianpiero.di.blasi\\codice\\Personale\\pizzApazzAinfinity\\src\\",
+    "-out",
+    "C:\\Users\\gianpiero.di.blasi\\codice\\Personale\\pizzApazzAinfinity\\transpile\\",
+    "-w"
+  };
+
   public static void main(String[] args) throws Exception {
-    Josetta.main(new String[]{ /*"-in",
-      "C:\\Users\\gianpiero.di.blasi\\codice\\Personale\\pizzApazzAinfinity\\src\\giada\\pizzapazza\\math\\Z4Math.java",
-      "-out",
-      "C:\\Users\\gianpiero.di.blasi\\codice\\Personale\\pizzApazzAinfinity\\src\\giada\\pizzapazza\\math\\Z4Math.js",
-      "-w",*/ /*"-in",
-      "C:\\Users\\gianpiero.di.blasi\\codice\\Personale\\pizzApazzAinfinity\\src\\",
-      "-out",
-      "C:\\Users\\gianpiero.di.blasi\\codice\\Personale\\pizzApazzAinfinity\\build\\js\\",
-      "-w"*/});
+    Josetta.main(args1);
   }
 }
