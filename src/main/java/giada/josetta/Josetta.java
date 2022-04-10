0fbabddeb31c400308e6c6c1ea093984043c4e5c
Gianpiero Di Blasi <32394331+gianpierodiblasi@users.noreply.github.com>
2022-04-10 17:03:13 +0200

    new parameters

--- src/main/java/giada/josetta/Josetta.java
+++ src/main/java/giada/josetta/Josetta.java
@@ -36,11 +36,14 @@ public class Josetta {
    *
    * @param in The input file
    * @param out The output file
+   * @param ag The list of array setter methods
+   * @param as The list of array getter methods
+   * @param nt The list of no transpilation symbols
    * @throws Exception thrown if an error occurs
    */
-  public static void transpile(File in, File out) throws Exception {
+  public static void transpile(File in, File out, String[] ag, String[] as, String[] nt) throws Exception {
     String javaCode = Files.readString(in.toPath());
-    String esCode = Josetta.transpile(javaCode).replaceAll("\\R{3,}+", "\n");
+    String esCode = Josetta.transpile(javaCode, ag, as, nt).replaceAll("\\R{3,}+", "\n");
 
     out.getParentFile().mkdirs();
     Files.writeString(out.toPath(), esCode);
@@ -50,16 +53,19 @@ public class Josetta {
    * Transpiles a Java code into ES6 code
    *
    * @param javaCode The java code
+   * @param ag The list of array setter methods
+   * @param as The list of array getter methods
+   * @param nt The list of no transpilation symbols
    * @return The ES6 code
    * @throws Exception thrown if an error occurs
    */
-  public static String transpile(String javaCode) throws Exception {
+  public static String transpile(String javaCode, String[] ag, String[] as, String[] nt) throws Exception {
     CompilationUnit compilationUnit = StaticJavaParser.parse(javaCode);
 
     Josetta.codeCleaning(compilationUnit);
-    JosettaChecker.checkCompilationUnit(compilationUnit);
+    JosettaChecker.checkCompilationUnit(compilationUnit, ag, as, nt);
 
-    JosettaPrinterVisitor visitor = new JosettaPrinterVisitor();
+    JosettaPrinterVisitor visitor = new JosettaPrinterVisitor(ag, as, nt);
     compilationUnit.accept(visitor, null);
     return visitor.toString();
   }
@@ -70,21 +76,21 @@ public class Josetta {
   }
 
   @SuppressWarnings("UseOfSystemOutOrSystemErr")
-  private static void transpileDir(File in, File out) throws Exception {
+  private static void transpileDir(File in, File out, String[] ag, String[] as, String[] nt) throws Exception {
     if (!in.exists() || in.isHidden()) {
     } else if (in.isDirectory()) {
       for (File child : in.listFiles()) {
-        Josetta.transpileDir(child, new File(out, child.getName()));
+        Josetta.transpileDir(child, new File(out, child.getName()), ag, as, nt);
       }
     } else if (in.isFile() && in.getName().endsWith(".java")) {
       out = new File(out.getParentFile(), out.getName().replace(".java", ".js"));
       System.out.println("transpiling " + in + " into " + out);
-      Josetta.transpile(in, out);
+      Josetta.transpile(in, out, ag, as, nt);
     }
   }
 
   @SuppressWarnings("UseOfSystemOutOrSystemErr")
-  private static void watch(File in, File out) throws Exception {
+  private static void watch(File in, File out, String[] ag, String[] as, String[] nt) throws Exception {
     System.out.println("watching " + in + " into " + out);
 
     Path inPath = in.toPath();
@@ -126,11 +132,11 @@ public class Josetta {
               throw new RuntimeException(ex.getMessage(), ex);
             }
           } else {
-            Josetta.transpileInWatch(inEventFile, outEventFile);
+            Josetta.transpileInWatch(inEventFile, outEventFile, ag, as, nt);
           }
         } else if (kind == StandardWatchEventKinds.ENTRY_MODIFY) {
           if (inEventFile.isFile()) {
-            Josetta.transpileInWatch(inEventFile, outEventFile);
+            Josetta.transpileInWatch(inEventFile, outEventFile, ag, as, nt);
           }
         } else if (kind == StandardWatchEventKinds.ENTRY_DELETE) {
           if (outEventFile.isDirectory()) {
@@ -150,14 +156,14 @@ public class Josetta {
     }
   }
 
-  @SuppressWarnings("UseOfSystemOutOrSystemErr")
-  private static void transpileInWatch(File inFile, File outFile) {
+  @SuppressWarnings({"UseOfSystemOutOrSystemErr", "CallToPrintStackTrace"})
+  private static void transpileInWatch(File inFile, File outFile, String[] ag, String[] as, String[] nt) {
     if (inFile.isFile() && inFile.getName().endsWith(".java")) {
       outFile = new File(outFile.getParentFile(), outFile.getName().replace(".java", ".js"));
 
       try {
         System.out.println("transpiling " + inFile + " into " + outFile);
-        Josetta.transpile(inFile, outFile);
+        Josetta.transpile(inFile, outFile, ag, as, nt);
       } catch (Exception ex) {
         System.out.println(ex.getMessage());
         ex.printStackTrace();
@@ -165,7 +171,7 @@ public class Josetta {
     }
   }
 
-  @SuppressWarnings("UseOfSystemOutOrSystemErr")
+  @SuppressWarnings({"UseOfSystemOutOrSystemErr", "CallToPrintStackTrace"})
   private static void delete(File file) {
     try {
       if (!file.exists()) {
@@ -184,22 +190,29 @@ public class Josetta {
     }
   }
 
-  @SuppressWarnings("UseOfSystemOutOrSystemErr")
+  @SuppressWarnings({"UseOfSystemOutOrSystemErr", "CallToPrintStackTrace"})
   public static void main(String[] args) {
     Options options = new Options();
     options.addOption(Option.builder("in").hasArg().desc("Input dir/file").argName("in").required().build());
     options.addOption(Option.builder("out").hasArg().desc("Output dir/file").argName("out").required().build());
     options.addOption(Option.builder("w").desc("Watch for files changes").argName("w").build());
+    options.addOption(Option.builder("ag").desc("Array getter methods").argName("ag").build());
+    options.addOption(Option.builder("as").desc("Array setter methods").argName("as").build());
+    options.addOption(Option.builder("nt").desc("No transpilation symbols").argName("nt").build());
 
     try {
       CommandLine cmd = new DefaultParser().parse(options, args);
       File in = new File(cmd.getOptionValue("in"));
       File out = new File(cmd.getOptionValue("out"));
 
+      String ag[] = cmd.hasOption("ag") ? cmd.getOptionValue("ag").split(",") : new String[]{"$get"};
+      String as[] = cmd.hasOption("as") ? cmd.getOptionValue("as").split(",") : new String[]{"$set"};
+      String nt[] = cmd.hasOption("nt") ? cmd.getOptionValue("nt").split(",") : new String[]{"$"};
+
       if (cmd.hasOption("w")) {
-        Josetta.watch(in, out);
+        Josetta.watch(in, out, ag, as, nt);
       } else {
-        Josetta.transpileDir(in, out);
+        Josetta.transpileDir(in, out, ag, as, nt);
       }
     } catch (ParseException ex) {
       System.out.println(ex.getMessage());
